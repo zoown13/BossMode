19bc9cec4f761bf5b3b90fd4e775759c5d024392
zoown13 <41746120+zoown13@users.noreply.github.com>
2020-02-10 17:40:50 +0900

    Initial commit

---
+++ Source/BossMode/BossMode.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BossMode.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BossMode, "BossMode" );
---
+++ Source/BossMode/BossModeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BossModeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOSSMODE_API ABossModeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
